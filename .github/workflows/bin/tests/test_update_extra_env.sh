d5bbecb779390765cc8029c4f5f97097bf288563
Kareem <kareem@grover.com>
2022-07-07 09:32:15 +0400

    fix: stuff

--- .github/workflows/bin/tests/test_update_extra_env.sh
+++ .github/workflows/bin/tests/test_update_extra_env.sh
@@ -1,8 +1,65 @@
 #!/bin/bash
 
-export FILE_NAME = ""
+export SERVICE_FILE_NAME="$(dirname $0)/.tmp-gateway.yaml"
+rm $SERVICE_FILE_NAME || true
 
-echo """
+function testExtraEnv {
+  echo "$3" > $SERVICE_FILE_NAME
+  EXTRA_ENV_KEY=$1 EXTRA_ENV_VALUE=$2 ../update_extra_env.sh
+  fileContent=$(cat $SERVICE_FILE_NAME)
+  [ "$fileContent" == "$4" ] && echo 'Test succeeded' || (echo -e "Test failed!\n\n--> Received:\n$fileContent\n\n--> Expected:\n$4")
+}
+
+###############################
+## Without extranEnv
+###############################
+before="service: graphql-gateway"
+after=$(cat << EOF
 service: graphql-gateway
-""" |
+extraEnv:
+  - name: SCHEMA_ID
+    value: 123
+EOF
+)
+testExtraEnv "SCHEMA_ID" "123" "$before" "$after"
+
+###############################
+## With extraEnv and key doesn't exist
+###############################
+before=$(cat << EOF
+service: graphql-gateway
+extraEnv:
+  - name: ENV
+    value: development
+EOF
+)
+after=$(cat << EOF
+service: graphql-gateway
+extraEnv:
+  - name: ENV
+    value: development
+  - name: SCHEMA_ID
+    value: 123
+EOF
+)
+testExtraEnv "SCHEMA_ID" "123" "$before" "$after"
+
+###############################
+## With extraEnv and key exists
+###############################
+before=$(cat << EOF
+service: graphql-gateway
+extraEnv:
+  - name: SCHEMA_ID
+    value: 12345
+EOF
+)
+after=$(cat << EOF
+service: graphql-gateway
+extraEnv:
+  - name: SCHEMA_ID
+    value: 123
+EOF
+)
+testExtraEnv "SCHEMA_ID" "123" "$before" "$after"
 
--- .github/workflows/bin/update_extra_env.sh
+++ .github/workflows/bin/update_extra_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yqs='(.extraEnv | .[(to_entries | .[] | select(.value.name == env(EXTRA_ENV_KEY)).key) // length]) += {"name": env(EXTRA_ENV_KEY), "value": env(EXTRA_ENV_VALUE)}'
 
-yq -i "$yqs" $FILE_NAME
+yq -i "$yqs" $SERVICE_FILE_NAME
 
