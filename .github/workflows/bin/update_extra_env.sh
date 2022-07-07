dc4918ffd32cfb7ce919852f8a1ca76b1f151764
Kareem <kareem@grover.com>
2022-07-07 08:57:33 +0400

    fix: stuff

--- .github/workflows/bin/update_extra_env.sh
+++ .github/workflows/bin/update_extra_env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 yqs='(.extraEnv | .[(to_entries | .[] | select(.value.name == env(EXTRA_ENV_KEY)).key) // length]) += {"name": env(EXTRA_ENV_KEY), "value": env(EXTRA_ENV_VALUE)}'
+
 yq -i "$yqs" kubernetes/applications/grover-apps/values-$ENVIRONMENT/$APP_NAME.yaml
 
